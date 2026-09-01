fe5f74ec116944b7921471596d6529045ebb2230
marquesmaycon <mayconmarquesh@gmail.com>
2026-09-01 01:25:02 -0300

    tailwind class sorter and simple import sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 //  @ts-check
-
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { tanstackConfig } from '@tanstack/eslint-config'
 
 export default [
   ...tanstackConfig,
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
     rules: {
       'import/no-cycle': 'off',
       'import/order': 'off',
@@ -12,6 +15,8 @@ export default [
       '@typescript-eslint/array-type': 'off',
       '@typescript-eslint/require-await': 'off',
       'pnpm/json-enforce-catalog': 'off',
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
   {
